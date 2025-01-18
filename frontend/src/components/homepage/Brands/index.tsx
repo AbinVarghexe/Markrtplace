69fe41b811d1e3be5a479247549b2f059f610518
ABIN-VARGEXE <abinvarghese2027@cs.ajce.in>
2025-01-19 02:55:46 +0530

    Remove favicon.ico and update Brands component heading styles for improved responsiveness

--- frontend/src/components/homepage/Brands/index.tsx
+++ frontend/src/components/homepage/Brands/index.tsx
@@ -5,7 +5,7 @@ const Brands = () => {
   return (
     <div className="bg-black">
       <div className="max-w-frame mx-auto flex flex-wrap items-center justify-center py-5 md:py-4 sm:px-4 xl:px-0 space-x-7">
-      <h1 className="text-white items-center text-[20px] md:text-[60px] font-black">
+      <h1 className="text-white items-center text-[25px] md:text-[40px] font-black">
       Exclusive for Amal jyothi students
       </h1>
       </div>
