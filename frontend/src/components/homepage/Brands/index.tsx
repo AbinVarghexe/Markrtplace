88062d80c08b6ca32d546383eb00259c72212367
ABIN-VARGEXE <abinvarghese2027@cs.ajce.in>
2025-01-18 14:46:22 +0530

    Add initial project structure with configuration files and type definitions

---
+++ frontend/src/components/homepage/Brands/index.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React from "react";
+
+const Brands = () => {
+  return (
+    <div className="bg-black">
+      <div className="max-w-frame mx-auto flex flex-wrap items-center justify-center py-5 md:py-4 sm:px-4 xl:px-0 space-x-7">
+      <h1 className="text-white items-center text-[20px] md:text-[60px] font-black">
+      Exclusive for Amal jyothi students
+      </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Brands;
